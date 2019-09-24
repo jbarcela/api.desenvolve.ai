5a374aa2faae1661095dc9cf2372733370a5a690
Nicolas Souza <nicolas.souza@viaflow.com.br>
2019-09-24 17:28:53 -0300

    Operations on receiver and donator user

--- src/modules/feedback/feedback.service.ts
+++ src/modules/feedback/feedback.service.ts
@@ -4,6 +4,7 @@ import { FeedbackInDto } from './dto/feedback-in.dto';
 import { Feedback } from './feedback.entity';
 import { UserRepository } from '../user/user.repository';
 import { InsufficientPointsException } from '../../exceptions/InsufficientPointsException';
+import { User } from '../user/user.entity';
 
 @Injectable()
 export class FeedbackService {
@@ -17,6 +18,8 @@ export class FeedbackService {
         if (await this._validatePointsToDonate(feedbackInDto)) {
             throw new InsufficientPointsException();
         }
+        this._updateDonatorUserPoints(feedbackInDto);
+        this._updateReceiverUserPoints(feedbackInDto);
         return this.feedbackRepository.save(feedback);
     }
 
@@ -39,5 +42,19 @@ export class FeedbackService {
             id: feedbackInDto.userDonatorId,
         });
         return user.points < feedbackInDto.points;
+    }
+
+    private async _updateDonatorUserPoints(feedbackInDto: FeedbackInDto) {
+        const user: User = await this.userRepository.findOne({id: feedbackInDto.userDonatorId});
+        user.points = +user.points - +feedbackInDto.points;
+
+        await this.userRepository.save(user);
+    }
+
+    private async _updateReceiverUserPoints(feedbackInDto: FeedbackInDto) {
+        const user: User = await this.userRepository.findOne({id: feedbackInDto.userReceiverId});
+        user.points = +user.points + +feedbackInDto.points;
+
+        await this.userRepository.save(user);
     }
 }
